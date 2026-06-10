a36c0eddbbbb8f25a1dd44d019975f5790835e79
Leon <kaizenworkflow@proton.me>
2026-06-10 14:02:09 +1000

    Fix server.js path resolution and add Pro features

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// Serve static files from "public" folder
-app.use(express.static(path.join(__dirname, "public")));
+// Serve static files from "public" folder (use absolute path)
+const publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
 
 // When someone goes to "/", send them index.html
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(publicPath, "index.html"));
 });
 
 const PORT = 4000;
